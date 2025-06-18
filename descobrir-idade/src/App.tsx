e8eadbb84384a35a53b82ebc4105dc9a62567945
leoporto166 <leonardo.porto166@gmail.com>
2025-06-18 15:44:53 -0300

    descobrir-idade completo

--- descobrir-idade/src/App.tsx
+++ descobrir-idade/src/App.tsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useState, type FormEvent } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputNome, setInputNome] = useState<string>("")
+  const [inputAno, setInputAno] = useState<number | "">("")
+  const [result, setResult] = useState<number>()
+
+  function Descobrir(event: FormEvent){
+    event.preventDefault();
+    const anoAtual = new Date().getFullYear();
+    if(inputAno !== ""){
+    const resultado = anoAtual - inputAno
+    setResult(resultado)
+    console.log(resultado)
+  }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container'>
+      <form className='formulario' onSubmit={Descobrir}>
+        <label>Digite seu nome</label>
+        <input
+        required
+        value={inputNome}
+        onChange={(e) => setInputNome(e.target.value)}
+        ></input>
+        <label>Digite seu ano de nascimento</label>
+        <input
+        type="number"
+        required
+        value={inputAno}
+        onChange={(e) => setInputAno(Number(e.target.value))}></input>
+
+        <button>Descobrir</button>
+      </form>
+
+      {result !== undefined && (<div className='resultado'
+      ><h1>Olá {inputNome} você tem {result} anos</h1>
+      </div>)}
+
+      
+    </div>
   )
 }
 
